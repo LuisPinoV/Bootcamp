99b48f50fe19b747d9199e1454c1cbd08bb1a194
ShamuelsRevenge <168758155+ShamuelsRevenge@users.noreply.github.com>
2024-07-03 13:29:01 -0400

    Merged Backend_C with Main

--- Frontend2_vb/Frontend2_vb/InfoSV.vb
+++ Frontend2_vb/Frontend2_vb/InfoSV.vb
@@ -1,25 +1,37 @@
-﻿Public Class InfoSV
-    Private Sub InfoSV_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Imports System.IO
+Imports System.Net
+Imports System.Net.Http
+Imports System.Text.Json
+Imports Microsoft
+Imports Newtonsoft.Json
+
+Public Class InfoSV
+    'sacado de
+    'https://es.stackoverflow.com/questions/436604/api-rest-en-visual-basic-cliente-windows-forms-vb-net
+    Private Async Sub InfoSV_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Dim respuesta As String = Await GetHttp("")
+        Dim lista As List(Of PacienteDatos) = JsonConvert.DeserializeObject(Of List(Of PacienteDatos))(respuesta)
+
+        'hacer q filtre por columna
+
+        ComboBox1.DataSource = "x"
+        DataGridView1.DataSource = lista
 
     End Sub
 
-    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
-        Dim font As New Font("Arial", 24, FontStyle.Bold)
-        Dim brush As New SolidBrush(Color.Black)
-        Dim point As New Point(350, 50)
-        e.Graphics.DrawString("VitaLuxx", font, brush, point)
-        font.Dispose()
-        brush.Dispose()
+
+    Private Async Function GetHttp(entidad As String) As Task(Of String) 'entidad sera la url
+        Dim oRequest As WebRequest = WebRequest.Create("https://localhost:7101/" + entidad)
+        Dim oResponse As WebResponse = oRequest.GetResponse()
+        Dim sr As StreamReader = New StreamReader(oResponse.GetResponseStream())
+        Return Await sr.ReadToEndAsync()
+    End Function
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
     End Sub
 
-    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
 
     End Sub
-    Private Sub Form3_campana(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim imagePath As String = "C:\Users\Larsi\OneDrive\Escritorio\campana.png"
-        PictureBox1.Image = Image.FromFile(imagePath)
-        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
-        PictureBox1.Location = New Point(750, 50)
-    End Sub
-
 End Class
