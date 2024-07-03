09f0c6f9687a8f3591d3738bba0127549385532b
LuisPinoV <144037119+LuisPinoV@users.noreply.github.com>
2024-07-03 12:56:58 -0400

    diseño muy muy inicial
    mis cambios contienen un titulo y una campanita :D

--- Frontend2_vb/Frontend2_vb/Form2.vb
+++ Frontend2_vb/Frontend2_vb/Form2.vb
@@ -23,4 +23,23 @@
 
 
     End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        Dim font As New Font("Arial", 24, FontStyle.Bold)
+        Dim brush As New SolidBrush(Color.Black)
+        Dim point As New Point(500, 50)
+        e.Graphics.DrawString("VitaLuxx", font, brush, point)
+        font.Dispose()
+        brush.Dispose()
+    End Sub
+    Private Sub Form2_campana(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim imagePath As String = "C:\Users\Larsi\OneDrive\Escritorio\campana.png"
+        PictureBox1.Image = Image.FromFile(imagePath)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.Location = New Point(750, 50)
+    End Sub
+
 End Class
--- Frontend2_vb/Frontend2_vb/InfoSV.vb
+++ Frontend2_vb/Frontend2_vb/InfoSV.vb
@@ -1,3 +1,25 @@
 ﻿Public Class InfoSV
+    Private Sub InfoSV_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        Dim font As New Font("Arial", 24, FontStyle.Bold)
+        Dim brush As New SolidBrush(Color.Black)
+        Dim point As New Point(350, 50)
+        e.Graphics.DrawString("VitaLuxx", font, brush, point)
+        font.Dispose()
+        brush.Dispose()
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+    Private Sub Form3_campana(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim imagePath As String = "C:\Users\Larsi\OneDrive\Escritorio\campana.png"
+        PictureBox1.Image = Image.FromFile(imagePath)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.Location = New Point(750, 50)
+    End Sub
 
 End Class
